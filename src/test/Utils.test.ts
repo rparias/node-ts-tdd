82a815bdb8e9e548da950eabcac09c129a912846
Ronald Arias <ariasron@hotmail.com>
2021-06-25 21:00:34 -0500

    Adds files for start testing them

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,10 +1,13 @@
-import {Utils} from "../app/Utils";
+import { Utils} from "../app/Utils/Utils";
 
 describe('Utils test suite', () => {
 
-    test('first test', () => {
-        const result = Utils.toUpperCase('abc');
-        expect(result).toBe('ABC');
+    beforeEach(() => {
+        console.log('before each');
+    });
+
+    beforeAll(() => {
+        console.log('before all');
     });
 
     test('parse simple URL', () => {
@@ -12,37 +15,38 @@ describe('Utils test suite', () => {
         expect(parsedUrl.href).toBe('http://localhost:8080/login');
         expect(parsedUrl.port).toBe('8080');
         expect(parsedUrl.protocol).toBe('http:');
-        expect(parsedUrl.query).toEqual({}); // toEqual is for objects, and toBe is for primitive values
+        expect(parsedUrl.query).toEqual({});
     });
 
     test('parse URL with query', () => {
+        const parsedUrl = Utils.parseUrl('http://localhost:8080/login?user=user&password=pass');
         const expectedQuery = {
             user: 'user',
             password: 'pass'
         }
-        const parsedUrl = Utils.parseUrl('http://localhost:8080/login?user=user&password=pass');
         expect(parsedUrl.query).toEqual(expectedQuery);
+        expect(expectedQuery).toBe(expectedQuery);
     });
 
     test('test invalid URL', () => {
         function expectError() {
-            Utils.parseUrl('');
+            Utils.parseUrl('')
         }
-        expect(expectError).toThrowError('Empty URL!');
+        expect(expectError).toThrow('Empty url');
     });
 
     test('test invalid URL with arrow function', () => {
         expect(() => {
             Utils.parseUrl('')
-        }).toThrowError('Empty URL!');
+        }).toThrow('Empty url');
     });
 
-    test('test invalid URL with try catch', () => {
+    test.only('test invalid URL with try catch', () => {
         try {
             Utils.parseUrl('');
         } catch (error) {
             expect(error).toBeInstanceOf(Error);
-            expect(error).toHaveProperty('message', 'Empty URL!');
+            expect(error).toHaveProperty('message', 'Empty url!');
         }
     });
 
