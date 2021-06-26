5f708230149c491f26d252872da8db02f9d29f05
Ronald Arias <ariasron@hotmail.com>
2021-06-25 22:03:21 -0500

    Creates test for LoginHandler using mock for response

---
+++ src/test/Handlers/LoginHandler.test.ts
@@ -0,0 +1,27 @@
+import {LoginHandler} from "../../app/Handlers/LoginHandler";
+import {HTTP_CODES, HTTP_METHODS} from "../../app/Models/ServerModels";
+import {IncomingMessage, ServerResponse} from "http";
+import {Authorizer} from "../../app/Authorization/Authorizer";
+
+
+describe('LoginHandler test suite' , () => {
+    let loginHandler: LoginHandler;
+
+    const requestMock = {
+        method: ''
+    } as IncomingMessage;
+    const responseMock = {
+        writeHead: jest.fn()
+    } as any;
+    const authorizerMock = {} as Authorizer;
+
+    beforeEach(() => {
+        loginHandler = new LoginHandler(requestMock, responseMock, authorizerMock);
+    });
+
+    test('options request', async () => {
+        requestMock.method = HTTP_METHODS.OPTIONS;
+        await loginHandler.handleRequest();
+        expect(responseMock.writeHead).toBeCalledWith(HTTP_CODES.OK);
+    });
+})
