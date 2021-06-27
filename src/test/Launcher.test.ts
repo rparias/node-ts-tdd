1e6b9466db1d27e35d8e428cd5283d83d0168bae
Ronald Arias <ariasron@hotmail.com>
2021-06-27 15:09:38 -0500

    Testing launcher using deep mocks

---
+++ src/test/Launcher.test.ts
@@ -0,0 +1,33 @@
+import {mocked} from "ts-jest/utils";
+import { Launcher } from "../app/Launcher";
+import { Server } from "../app/Server/Server";
+jest.mock('../app/Server/Server', () => {
+    return {
+        Server: jest.fn(() => {
+            return {
+                startServer: () => {
+                    console.log('starting fake server');
+                }
+            }
+        }),
+    }
+});
+
+describe('Launcher test suite', () => {
+    const mockedServer = mocked(Server, true);
+
+    test('create server', () => {
+        new Launcher();
+        expect(mockedServer).toBeCalled();
+    });
+
+    test('launch app', () => {
+        const launcherMock = jest.fn();
+        Launcher.prototype.launchApp = launcherMock;
+        const launcher = new Launcher();
+
+        launcher.launchApp();
+
+        expect(launcherMock).toBeCalled();
+    })
+})
