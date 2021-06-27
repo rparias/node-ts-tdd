5428b619a086ae457313e033fb0c1f4e2f0db144
Ronald Arias <ariasron@hotmail.com>
2021-06-27 14:51:07 -0500

    Testing callbacks with jest matchers, expect.any y prototype injection on SessionTokenDbAccess

--- src/test/Authorization/SessionTokenDBAccess.test.ts
+++ src/test/Authorization/SessionTokenDBAccess.test.ts
@@ -29,6 +29,11 @@ describe('SessionTokenDBAccess', () => {
         valid: true
     };
 
+    test('constructor argument call', async () => {
+        new SessionTokenDBAccess();
+        expect(Nedb).toBeCalledWith('databases/sessionToken.db');
+    });
+
     test('store sessionToken without error', async () => {
         // para este tipo de tests se necesita mockear la implementacion
         nedbMock.insert.mockImplementationOnce(
@@ -52,5 +57,44 @@ describe('SessionTokenDBAccess', () => {
         await expect(sessionTokenDBAccess.storeSessionToken(someToken)).rejects.toThrow('something went wrong!');
 
         expect(nedbMock.insert).toBeCalledWith(someToken, expect.any(Function));
+    });
+
+    test('get token with result and no error', async () => {
+        nedbMock.find.mockImplementationOnce(
+            (query: any, cb: any) => {
+                cb(null, [someToken]);
+            }
+        );
+        const someTokenId = '123';
+
+        const resultToken = await sessionTokenDBAccess.getToken(someTokenId);
+
+        expect(resultToken).toBe(someToken);
+        expect(nedbMock.find).toBeCalledWith({tokenId: someTokenId}, expect.any(Function));
+    });
+
+    test('get token with no result and no error', async () => {
+        nedbMock.find.mockImplementationOnce(
+            (query: any, cb: any) => {
+                cb(null, []);
+            }
+        );
+        const someTokenId = '123';
+
+        const resultToken = await sessionTokenDBAccess.getToken(someTokenId);
+
+        expect(resultToken).toBe(undefined);
+        expect(nedbMock.find).toBeCalledWith({tokenId: someTokenId}, expect.any(Function));
+    });
+
+    test('get token with error', async () => {
+        nedbMock.find.mockImplementationOnce(
+            (query: any, cb: any) => {
+                cb(new Error('something went wrong!'), null);
+            }
+        );
+
+        await expect(sessionTokenDBAccess.getToken('123')).rejects.toThrow('something went wrong!');
+        expect(nedbMock.find).toBeCalledWith({tokenId: '123'}, expect.any(Function));
     });
 })
